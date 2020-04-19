5deba5c05cf9d7a1603fcfea3dee1c7f18172beb
Salem Owino <salemowino18@gmail.com>
2020-04-19 13:50:41 +0300

    A test to detect new user

--- user_test.py
+++ user_test.py
@@ -5,7 +5,7 @@ from credentials import Credentials
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User("Salem", "Owino", "0757871491", "salemowino18@gmail.com", "salem", "leejones2")
+        self.new_user = User("Salem", "Owino", "0757871491", "salemowino18@gmail.com", "saa-lem", "leejones")
 
     def test_init(self):
         self.assertEqual(self.new_user.first_name,"Salem")
@@ -13,4 +13,20 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.phone_number,"0757871491")
         self.assertEqual(self.new_user.email_address,"salemowino18@gmail.com")
         self.assertEqual(self.new_user.username, "saa-lem")
+        self.assertEqual(self.new_user.password, "leejones1")
+
+
+
+    def tearDown(self):
+                
+        credentials_list = []
+        User.users_list = []
+
+
+    def test_save_multiple_userDetails(self):
+        self.new_user.save_userDetails()
+        test_user = User("test","user","0757871491","salemowino18@gmail.com","saa-lem","leejones1")
+        test_user.save_userDetails()
+        self.assertEqual(len(User.users_list),2)
+
 
